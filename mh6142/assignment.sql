f96b5445ec891a48af850879a5e608f841db96b6
lifengzhi <lifengzhi@e.ntu.edu.sg>
2019-09-28 14:10:23 +0800

    initial

---
+++ mh6142/assignment.sql
@@ -0,0 +1,11 @@
+
+later add the foreign key reference to club
+
+create table PLAYERS(
+    player_id int NOT NULL,
+    dob date NOT NULL,
+    position VARCHAR(64) NOT NULL,
+    salary float NOT NULL,
+    clubname VARCHAR(64)); 
+    
+
